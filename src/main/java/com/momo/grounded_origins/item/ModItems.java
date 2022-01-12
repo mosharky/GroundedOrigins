16f0335558f22eb7ba0293d45e4700b27b298073
mo_shark <medoelzarkalt@gmail.com>
2022-01-12 00:47:10 -0500

    Proper formatting

--- src/main/java/com/momo/grounded_origins/item/ModItems.java
+++ src/main/java/com/momo/grounded_origins/item/ModItems.java
@@ -14,11 +14,11 @@ public class ModItems {
     public static final RegistryObject<Item> BEACH_CRAB_ICON = ITEMS.register("beach_crab_icon",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> DEER_ICON = ITEMS.register("deer_icon",
-        () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> MOTH_ICON = ITEMS.register("moth_icon",
-        () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> PENGUIN_ICON = ITEMS.register("penguin_icon",
-        () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()));
 
 
     public static void register(IEventBus eventBus) {
