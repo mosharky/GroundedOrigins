91e22bbf7ac1f990ff4b389611372bc62ee5de09
mo_shark <medoelzarkalt@gmail.com>
2022-03-22 02:48:34 -0400

    File-structure changes and refactoring

---
+++ src/main/java/momo/grounded_origins/registry/Items.java
@@ -0,0 +1,48 @@
+package momo.grounded_origins.registry;
+
+import momo.grounded_origins.GroundedOrigins;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class Items {
+
+    // Icons
+    public static final Item BEACH_CRAB_ICON = new Item(new FabricItemSettings());
+    public static final Item DEER_ICON = new Item(new FabricItemSettings());
+    public static final Item MOTH_ICON = new Item(new FabricItemSettings());
+    public static final Item PENGUIN_ICON = new Item(new FabricItemSettings());
+
+    // Moth foods
+    public static final Item FLOWER_STAMENS = new Item(new FabricItemSettings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(8).saturationModifier(0.9f).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 20*120, 0, true, false), 1f).build())
+    );
+    public static final Item FIBROUS_STRING = new Item(new FabricItemSettings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(6).saturationModifier(0.8f).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 20*60, 0, true, false), 1f).build())
+    );
+    public static final Item PAPER_SHREDS = new Item(new FabricItemSettings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(4).saturationModifier(0.8f).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 20*60, 0, true, false), 1f).build())
+    );
+    public static final Item BOOK_BITE = new Item(new FabricItemSettings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(8).saturationModifier(0.8f).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 20*60, 0, true, false), 0.25f).build())
+    );
+
+    public static void init() {
+        // Icons
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "beach_crab_icon"), BEACH_CRAB_ICON);
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "deer_icon"), DEER_ICON);
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "moth_icon"), MOTH_ICON);
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "penguin_icon"), PENGUIN_ICON);
+        // Moth foods
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "flower_stamens"), FLOWER_STAMENS);
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "fibrous_string"), FIBROUS_STRING);
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "paper_shreds"), PAPER_SHREDS);
+        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "book_bite"), BOOK_BITE);
+    }
+}
